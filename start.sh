2efc4b01599373d35303d66a88275f87d77b191e
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-06-05 16:44:08 +0500

    Modify start.sh to run both fluentd and kube-gen at the same time

--- start.sh
+++ start.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
 
-# Convert COMMAND variable into an array
-# Simulating positional parameter behaviour
-IFS=' ' read -r -a CMD_ARRAY <<< "$COMMAND"
-
-# explicitly setting positional parameters ($@) to CMD_ARRAY
-set -- "${CMD_ARRAY[@]}"
-# From this point, positional parameters ($@)will be set to the parameters in the COMMAND variable.
-
-# chown home and data folder
 chown -R fluentd /fluentd
 
-# https://github.com/fluent/fluentd-docker-image/issues/48
-# Running fluentd as Root so that it can read logs from /var/log without permission issues.
-exec "$@"
+fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins $FLUENTD_OPT &
+/fluentd/etc/scripts/kube-gen -watch -type pods -wait 2s:3s -post-cmd '/etc/scripts/kill-processes.sh fluentd' /fluentd/etc/template/${FLUENTD_CONF_TEMPLATE} /fluentd/etc/${FLUENTD_CONF}
