2a6ef3348fa09dd6c06bfe9ef6a710db528c59ae
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-06-08 16:14:13 +0500

    Fix restart bug

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 set -e
 
 fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins $FLUENTD_OPT &
-/fluentd/etc/scripts/kube-gen -watch -type pods -wait 2s:3s -post-cmd '/fluentd/etc/scripts/kill-processes.sh fluentd' /fluentd/etc/template/${FLUENTD_CONF_TEMPLATE} /fluentd/etc/${FLUENTD_CONF}
+/fluentd/etc/scripts/kube-gen -watch -type pods -overwrite -wait 2s:3s -post-cmd '/fluentd/etc/scripts/kill-processes.sh fluentd' /fluentd/etc/template/${FLUENTD_CONF_TEMPLATE} /fluentd/etc/${FLUENTD_CONF}
